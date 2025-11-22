ec689ac6583eb9468ab835f948fbe256cfbfb98d
iago-silves <iago.rodrig.silvestre@gmail.com>
2025-11-22 20:29:48 -0300

    adiciona solução do exercicio

---
+++ src/funcional/lamdbdas/intermediario/FiltrarNomes.java
@@ -0,0 +1,30 @@
+package funcional.lamdbdas.intermediario;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FiltrarNomes {
+    public static void main(String[] args) {
+        List<String> nomes = List.of("Ana", "Bruno", "Carlos", "Amanda", "Bia");
+
+
+//        for(String nome: nomes){
+//            char primeiraLetra = nome
+//                    .toUpperCase()
+//                    .charAt(0);
+//
+//            if(primeiraLetra == 'A'){
+//                System.out.println(nome);
+//            }
+//        }
+//
+        List<String> nomesComA = nomes.stream()
+                .filter(nome -> nome.toUpperCase().charAt(0) == 'A')
+                .toList();
+
+        System.out.println(nomesComA);
+
+    }
+}
