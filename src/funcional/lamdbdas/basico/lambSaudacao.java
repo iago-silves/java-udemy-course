135a9a2dbbce093b246a3ce2dc693036cb55c62a
iago-silves <iago.rodrig.silvestre@gmail.com>
2025-11-22 15:24:18 -0300

    adiciona primeiro exerecicio

--- src/funcional/lamdbdas/basico/lambSaudacao.java
+++ src/funcional/lamdbdas/basico/lambSaudacao.java
@@ -1,4 +1,11 @@
 package funcional.lamdbdas.basico;
 
+import java.util.function.Consumer;
+
 public class lambSaudacao {
+    public static Consumer<String> saudacao = nome -> System.out.println("Seja bem-vindo: " + nome);
+
+    public static void main(String[] args) {
+        saudacao.accept("Iago Silvestre.");
+    }
 }
